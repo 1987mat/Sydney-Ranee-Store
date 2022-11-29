140352fdbfa75f19bf455dcccea347cce7117629
matcalvani <matstraty@gmail.com>
2022-11-28 19:15:16 -0800

    updates

--- functions.php
+++ functions.php
@@ -3,28 +3,20 @@
 function loadScripts() {
   wp_register_style('style', get_template_directory_uri() . '/css/style.css');
   wp_enqueue_style('style');
+  wp_enqueue_style( 'dashicons' );
   wp_enqueue_script('main-js', get_theme_file_uri('/build/index.js'), array(), '1.0', true);
 }
 
 add_action('wp_enqueue_scripts', 'loadScripts');
 
-// function remove_admin_bar_links() {
-//   global $wp_admin_bar;
-//   $wp_admin_bar->remove_menu('view-site');        // Remove the view site link
-//   // $wp_admin_bar->remove_menu('site-name');        // Remove the site name menu
-// }
-
-// add_action( 'wp_before_admin_bar_render', 'remove_admin_bar_links' );
-
-// Add WooCommerce support
 function mytheme_add_woocommerce_support() {
   add_theme_support( 'woocommerce' );
 }
 
+// Add WooCommerce support
 add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
 
-
-// Add support
+// Add nav menu & thumbnails
 add_theme_support('menus');
 add_theme_support('post-thumbnails');
 
@@ -36,24 +28,38 @@ register_nav_menus(
 
 add_image_size('post_image', 1100, 750, true);
 
-function load_dashicons_front_end() {
-  wp_enqueue_style( 'dashicons' );
-}
-
-// Enqueue the Dashicons script
-add_action( 'wp_enqueue_scripts', 'load_dashicons_front_end' );
-
+// Override Woocommerce CSS
 add_filter( 'woocommerce_enqueue_styles', '__return_false' );
 
+function add_script_to_footer(){
+  if(!is_admin()) { ?>
+  <script>
+    jQuery(document).ready(function($){
+      $(document).on('click', '.plus', function(e) { 
+        $input = $(this).prev('input.qty');
+        var val = parseInt($input.val());
+        var step = $input.attr('step');
+        step = 'undefined' !== typeof(step) ? parseInt(step) : 1;
+        $input.val( val + step ).change();
+        $('#update-cart-btn').addClass('active');
+      });
 
-// Add widget
-// register_sidebar(
-//   array(
-//     'name' => 'Page Sidebar',
-//     'id' => 'page-sidebar',
-//     'before_title' => '<h4>',
-//     'after_title' => '</h4>'
-//   )
-// );
+      $(document).on('click', '.minus',  
+        function(e) {
+        $input = $(this).next('input.qty');
+        var val = parseInt($input.val());
+        var step = $input.attr('step');
+        step = 'undefined' !== typeof(step) ? parseInt(step) : 1;
+        if (val > 0) {
+          $input.val( val - step ).change();
+          $('#update-cart-btn').addClass('active');
+        }
+      });
+    });
+  </script>
+<?php
+  }
+}
 
-
+// jQuery plus & minus button for quantity input
+add_action( 'wp_footer', 'add_script_to_footer');
