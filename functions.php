deb6a500043233b84ed0ac3ae597950d83c51b95
matcalvani <matstraty@gmail.com>
2022-12-19 18:03:34 -0800

    changes

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 function loadScripts() {
   wp_register_style('style', get_template_directory_uri() . '/css/style.css');
   wp_enqueue_style('style');
-  wp_enqueue_style( 'dashicons' );
+  wp_enqueue_style('dashicons');
   wp_enqueue_script('main-js', get_theme_file_uri('/build/index.js'), array(), '1.0', true);
 
    // Make url relative for AJAX requests
@@ -16,7 +16,7 @@ function loadScripts() {
 add_action('wp_enqueue_scripts', 'loadScripts');
 
 function mytheme_add_woocommerce_support() {
-  add_theme_support( 'woocommerce' );
+  add_theme_support('woocommerce');
 }
 
 // Add WooCommerce support
